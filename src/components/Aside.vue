762cc13d0d5ce1070723edd0161e319e27e4b353
Derek <jay15003939@gmail.com>
2024-08-10 14:45:50 +0800

    feat: init

---
+++ src/components/Aside.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-menu
+    default-active="2"
+    class="aside-container"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <p class="logo-lg">彩妍陪診</p>
+    <TreeMenu :menuData="menuData" :index="1" />
+  </el-menu>
+</template>
+
+<script setup>
+import TreeMenu from "./TreeMenu.vue";
+import { useRouter } from "vue-router";
+import { reactive } from "vue";
+const router = useRouter();
+
+const menuData = reactive(router.options.routes[0].children);
+
+const handleOpen = () => {};
+const handleClose = () => {};
+</script>
+
+<style scoped lang="scss">
+.aside-container {
+  background: rgba(#000, 0.4);
+  height: 100%;
+  .logo-lg {
+    font-size: 20px;
+    text-align: center;
+    height: 50px;
+    line-height: 50px;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <!-- <el-aside width="200px">Aside</el-aside> -->
+      <Aside />
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import Aside from "../components/Aside.vue";
+</script>
+
+<style lang="scss" scoped>
+.common-layout {
+  height: 100%;
+  .el-container {
+    height: 100%;
+  }
+}
+</style>
